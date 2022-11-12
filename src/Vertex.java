1d331a75f2c2f4a016a085e2c591b6df113a96f4
anguyen6262 <andrew.nguyen6262@gmail.com>
2022-11-11 18:34:34 -0600

    Adding in vertex and edge class

---
+++ src/Vertex.java
@@ -0,0 +1,11 @@
+import edu.macalester.graphics.Ellipse;
+
+public class Vertex extends Ellipse{
+    private double centerX;
+    private double centerY;
+
+    public Vertex(){
+    Ellipse vertex = new Ellipse(centerX, centerY);
+    }
+}
+

33f01f5dae2805d74f7ba1dccb611bf482ab5909
anguyen6262 <andrew.nguyen6262@gmail.com>
2022-11-22 20:07:39 -0600

    Text correctly displayed

--- src/haveliHakimi/State.java
+++ src/haveliHakimi/State.java
@@ -1,7 +1,6 @@
 package haveliHakimi;
 
 import java.awt.Color;
-import java.io.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -11,7 +10,6 @@ import java.util.Map;
 
 import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Ellipse;
-import edu.macalester.graphics.Point;
 import stackImplementation.ArrayStack;
 import edu.macalester.graphics.Line;
 import edu.macalester.graphics.GraphicsText;
@@ -33,8 +31,10 @@ public class State {
         int xPos = canvasWidth/4;
         double yPos = canvasHeight/2;
         for(Integer i = 0; i < numVertices; i++){
-            createVertex(canvas, i, xPos, yPos);
-            createText(canvas, i.toString(), xPos+11, yPos+20,colors.get(i));
+            Ellipse vertex = new Ellipse(xPos, yPos, 30, 30);
+            vertex.setFillColor(Color.black);
+            canvas.add(vertex);
+            verticesMap.put(i, vertex);
             xPos+=70;
             if(i % 2 ==1) {
                 yPos+=Math.ceil(Math.random()*200);
@@ -50,42 +50,36 @@ public class State {
             if(degreeSequence.get(i) > graph.degree(i)){
                 for(int j = 1; j < degreeSequence.size(); j++){
                     if(degreeSequence.get(j) > graph.degree(j)) {
-                        createEdge(canvas, graph, i, j);
+                        graph.addEdge(i, j);
+                        Line edge = new Line(verticesMap.get(i).getCenter(), verticesMap.get(j).getCenter());
+                        canvas.add(edge);
                     }
                 }
             }
         }
     }
-
-    private void createVertex(CanvasWindow canvas, int num, int xPos, double yPos) {
-        Ellipse vertex = new Ellipse(xPos, yPos, 30, 30);
-        vertex.setFillColor(Color.black);
-        canvas.add(vertex);
-        verticesMap.put(num, vertex);
-    }
-
-    private void createEdge(CanvasWindow canvas, Graph graph, int source, int target) {
-        graph.addEdge(source, target);
-        Line edge = new Line(verticesMap.get(source).getCenter(), verticesMap.get(target).getCenter());
-        canvas.add(edge);
-    }
     
-    private void createText(CanvasWindow canvas, String num, int xPos, double yPos, Color color) {
-        GraphicsText text = new GraphicsText(num, xPos, yPos);
-        text.setFillColor(color);
-        canvas.add(text);
+    private void drawText(CanvasWindow canvas) {
+        for(int i = 0; i < verticesMap.size(); i++){
+            verticesMap.get(i).getCenter().getX();
+            Integer num = i;
+            GraphicsText text = new GraphicsText(num.toString(), verticesMap.get(i).getCenter().getX()-4, verticesMap.get(i).getCenter().getY()+4);
+            text.setFillColor(colors.get(i));
+            canvas.add(text);
+        }
+    }
+
+    private void run(CanvasWindow canvas, int numVertices, Graph graph, ArrayList<Integer> degreeSequence ){
+        drawVertices(canvas, numVertices);
+        drawEdges(canvas, graph, degreeSequence);
+        drawText(canvas);
     }
 
     public static void main(String[] args) {
         CanvasWindow canvas = new CanvasWindow("State", 800, 600);
         State state = new State();
         Graph graph = new Graph(7);
-        // graph.addEdge(1, 4);
-        // graph.addEdge(2, 1);
-        // graph.addEdge(3, 4);
-        // graph.addEdge(3, 2);
-        state.drawVertices(canvas,graph.V());
-        state.drawEdges(canvas,graph, new ArrayList<>(Arrays.asList(4,5,4,3,3,3,4)));
+        state.run(canvas, graph.V(), graph, new ArrayList<>(Arrays.asList(4,5,4,3,3,3,4)));
         System.out.println(graph.toString());            
     }   
 }
